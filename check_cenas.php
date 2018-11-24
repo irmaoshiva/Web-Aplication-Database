174efe16578b8f9e9d402916699d011634ae4fc6
diogoegrodrigues <39026333+diogoegrodrigues@users.noreply.github.com>
2018-11-24 17:13:53 +0000

    Add files via upload

---
+++ check_cenas.php
@@ -0,0 +1,61 @@
+<html>
+<body>
+	<?php
+	$host = "db.tecnico.ulisboa.pt";
+	$user = "ist425330";
+	$pass = "acdo1863";
+	$dsn = "mysql:host=$host;dbname=$user";
+	try
+	{
+		$connection = new PDO($dsn, $user, $pass);
+	}
+	catch(PDOException $exception)
+	{
+		echo("<p>Error: ");
+		echo($exception->getMessage());
+		echo("</p>");
+		exit();
+	}
+	$VAT_client = $_REQUEST['VAT_client'];
+	$animal_name = $_REQUEST['animal_name'];
+	$owner_name = $_REQUEST['owner_name'];
+	$sql = "SELECT a.name as animal_name, p.name as owner_name FROM person p INNER JOIN animal a ON (p.VAT = a.VAT) WHERE a.name = '$animal_name' AND p.name like '%$owner_name%'";
+	/*echo("<p>$sql</p>");*/
+	$result = $connection->query($sql);
+	$nrows = $result->rowCount();
+	if ($nrows == 0)
+	{
+		echo("<h2>Animal Not Found</h2>");
+		echo("<h3>If you want to register the animal in the veterinary hospital, please enter the following data</h3>");
+		echo("<form action='new_animal.php' method='post'>
+				<p><input type=hidden name='VAT_client'
+			value='$VAT_client'/></p>
+				<p><input type=hidden name='animal_name'
+			value='$animal_name'/></p>
+				<p>Species Name: <input type='text' name='species_name'/></p>
+				<p>Colour: <input type='text' name='colour'/></p>
+				<p>Gender: <input type='text' name='gender'/></p>
+				<p>Birth Year: <input type='text' name='birth_year'/></p>
+				<p><small><u>NOTE:</u> Name and VAT owner were used the data previously entered. Age is automatically calculated.</small></p>		
+				<p><input type='submit' value='SUBMIT'/></p>
+			</form>");
+	}
+	else
+	{	
+		echo("<h2>Animal is already registered</h2>");
+		echo("<h3>Matching Animals</h3>");
+		echo("<table border=\"2\">");
+		echo("<tr><td>Animal Name</td><td>Owner Name</td></tr>");
+		foreach($result as $row)
+		{
+			echo("<tr>\n");
+			echo("<td>{$row['animal_name']}</td>\n");
+			echo("<td>{$row['owner_name']}</td>\n");
+			echo("</tr>\n");
+		}
+		echo("</table>");
+	}
+	$connection = null;
+	?>
+</body>
+</html>
---
+++ new_animal.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+	<?php
+	$host = "db.tecnico.ulisboa.pt";
+	$user = "ist425330";
+	$pass = "acdo1863";
+	$dsn = "mysql:host=$host;dbname=$user";
+	try
+	{
+		$connection = new PDO($dsn, $user, $pass);
+	}
+	catch(PDOException $exception)
+	{
+		echo("<p>Error: ");
+		echo($exception->getMessage());
+		echo("</p>");
+		exit();
+	}
+	$animal_name = $_REQUEST['animal_name'];
+	$VAT_client = $_REQUEST['VAT_client'];
+	$species_name = $_REQUEST['species_name'];
+	$colour = $_REQUEST['colour'];
+	$gender = $_REQUEST['gender'];
+	$birth_year = $_REQUEST['birth_year'];
+	$sql = "INSERT INTO animal VALUES ('$animal_name', $VAT_client, '$species_name', '$colour', '$gender', '$birth_year', 10)";
+	/*echo("<p>$sql</p>");*/
+	$nrows = $connection->exec($sql);
+	/*echo("<p>Rows inserted: $nrows</p>");*/
+	if(nrows)
+		echo("<h2>Animal Successfully Registered</h2>");
+	$connection = null;
+	?>
+</body>
+</html>
