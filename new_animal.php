56c9a3d9d132813f0907c0e48cd1405f152584d8
diogoegrodrigues <diogoegrodrigues@gmail.com>
2018-11-30 09:21:38 +0000

    olaaaa

--- new_animal.php
+++ new_animal.php
@@ -23,10 +23,10 @@
 	$gender = $_REQUEST['gender'];
 	$birth_year = $_REQUEST['birth_year'];
 	$sql = "INSERT INTO animal VALUES ('$animal_name', $VAT_client, '$species_name', '$colour', '$gender', '$birth_year', 10)";
-	/*echo("<p>$sql</p>");*/
+	echo("<p>$sql</p>");
 	$nrows = $connection->exec($sql);
-	/*echo("<p>Rows inserted: $nrows</p>");*/
-	if(nrows)
+	echo("<p>Rows inserted: $nrows</p>");
+	if($nrows)
 		echo("<h2>Animal Successfully Registered</h2>");
 	$connection = null;
 	?>
