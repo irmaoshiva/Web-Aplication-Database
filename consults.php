2dee99e40591a64566629453ddd2710c2142d931
irmaoshiva <rsmoreira.pedro@gmail.com>
2018-12-03 13:41:55 +0000

    hyperlink p/ procedures.php

--- consults.php
+++ consults.php
@@ -25,28 +25,11 @@
 	/*echo("<p>$sql</p>");*/ 
 	$result = $connection->query($sql);
 	$nrows = $result->rowCount();
+	
+	
 	if ($nrows == 0)
 	{
 		echo("<h2>Previous Consults Not Found</h2>");
-		echo("<h3>If you want to register a consult, please enter the following data</h3>");
-		echo("<form action='new_consult.php' method='post'>
-				<p><input type=hidden name='VAT_client'
-			value='$VAT_client'/></p>
-				<p><input type=hidden name='animal_name'
-			value='$animal_name'/></p>
-				<p>Date(Y-M-D): <input type='text' name='date_timestamp'/></p>				
-				<p>S: <input type='text' name='S'/></p>
-				<p>O: <input type='text' name='O'/></p>
-				<p>A: <input type='text' name='A'/></p>
-				<p>P: <input type='text' name='P'/></p>
-				<p>VAT_Owner: <input type='text' name='VAT_owner'/></p>
-				<p>VAT_Veterinary: <input type='text' name='VAT_vet'/></p>
-				<p>Weight: <input type='text' name='weight'/></p>
-				<p>Diagnostic codes: <input type='text' name='code'/></p>
-
-				<p><small><u>NOTE:</u> Name and VAT owner were used the data previously entered.</small></p>		
-				<p><input type='submit' value='SUBMIT'/></p>
-			</form>");
 	}
 	else
 	{	
@@ -60,30 +43,35 @@
 			echo("<td>{$row['date_timestamp']}</td>\n");
 			echo("<td><a href=\"consult_details.php?VAT_client=$VAT_client&animal_name=$animal_name&date_timestamp={$row['date_timestamp']}");
 			echo("\">More information</a></td>\n");
+			echo("<td><a href=\"procedures.php?VAT_client=$VAT_client&animal_name=$animal_name&date_timestamp={$row['date_timestamp']}");
+			echo("\">Insert Procedure</a></td>\n");
 			echo("</tr>\n");
 		}
-
 		echo("</table>");
-		echo("<h3>If you want to register a consult, please enter the following data</h3>");
-		echo("<form action='new_consult.php' method='post'>
-				<p><input type=hidden name='VAT_client'
-			value='$VAT_client'/></p>
-				<p><input type=hidden name='animal_name'
-			value='$animal_name'/></p>
-				<p>Date(Y-M-D): <input type='text' name='date_timestamp'/></p>				
-				<p>S: <input type='text' name='S'/></p>
-				<p>O: <input type='text' name='O'/></p>
-				<p>A: <input type='text' name='A'/></p>
-				<p>P: <input type='text' name='P'/></p>
-				<p>VAT_Owner: <input type='text' name='VAT_owner'/></p>
-				<p>VAT_Veterinary: <input type='text' name='VAT_vet'/></p>
-				<p>Weight: <input type='text' name='weight'/></p>
-				<p>Diagnostic codes: <input type='text' name='code'/></p>
-
-				<p><small><u>NOTE:</u> Name and VAT owner were used the data previously entered.</small></p>		
-				<p><input type='submit' value='SUBMIT'/></p>
-			</form>");
 	}
+	
+		
+	echo("<h3>If you want to register a consult, please enter the following data</h3>");
+	echo("<form action='new_consult.php' method='post'>
+			<p><input type=hidden name='VAT_client'
+		value='$VAT_client'/></p>
+			<p><input type=hidden name='animal_name'
+		value='$animal_name'/></p>
+			<p>Date(Y-M-D): <input type='text' name='date_timestamp'/></p>				
+			<p>S: <input type='text' name='S'/></p>
+			<p>O: <input type='text' name='O'/></p>
+			<p>A: <input type='text' name='A'/></p>
+			<p>P: <input type='text' name='P'/></p>
+			<p>VAT_Owner: <input type='text' name='VAT_owner'/></p>
+			<p>VAT_Veterinary: <input type='text' name='VAT_vet'/></p>
+			<p>Weight: <input type='text' name='weight'/></p>
+			<p>Diagnostic codes: <input type='text' name='code'/></p>
+
+			<p><small><u>NOTE:</u> Name and VAT owner were used the data previously entered.</small></p>		
+			<p><input type='submit' value='SUBMIT'/></p>
+		</form>");
+
+	
 	$connection = null;
 	?>
 </body>
