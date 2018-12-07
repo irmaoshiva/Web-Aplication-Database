92e14c40ddcafebe213a5785b822f4f8139c13ce
irmaoshiva <rsmoreira.pedro@gmail.com>
2018-12-07 00:54:05 +0000

    hh

--- consults.php
+++ consults.php
@@ -2,8 +2,8 @@
 <body>
 	<?php
 	$host = "db.ist.utl.pt";
-	$user = "istxxxxxx";
-	$pass = "xxxxxxxx";
+	$user = "ist426527";
+	$pass = "hith1616";
 	$dsn = "mysql:host=$host;dbname=$user";
 	try
 	{
