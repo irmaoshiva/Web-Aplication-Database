c7bd07ca6b755d1686c286e2b8ba83e3c7b6f756
irmaoshiva <rsmoreira.pedro@gmail.com>
2018-12-07 00:53:22 +0000

    ff

--- check.php
+++ check.php
@@ -2,8 +2,8 @@
 <body>
 	<?php
 	$host = "db.tecnico.ulisboa.pt";
-	$user = "istxxxxxx";
-	$pass = "xxxxxxxx";
+	$user = "ist426527";
+	$pass = "hith1616";
 	$dsn = "mysql:host=$host;dbname=$user";
 	try
 	{
